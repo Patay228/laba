10d8587bd3110b3c013a3b5f3c3d56d57c5654af
Vladimir Pashkovskiy <dedytubbe@gmail.com>
2023-11-13 21:55:40 +0300

    laba14(2)kon

--- laba14(2).py
+++ laba14(2).py
@@ -1,22 +1,25 @@
 s=input()
-stack=[]
-sight=True
-for i in s:
-    if i in '([{':
-        stack.append(i)
-    elif i in ')]}':
-        if len(stack)==0:
+if "(" in s or ")" in s or "[" in s or "]" in s or "{" in s or "}" in s:
+    stack=[]
+    sight=True
+    for i in s:
+        if i in '([{':
+            stack.append(i)
+        elif i in ')]}':
+            if len(stack)==0:
+                sight=False
+                break
+            open=stack.pop()
+            if open=="(" and i==")":
+                continue
+            if open=="{" and i=="}":
+                continue
+            if open=="[" and i=="]":
+                continue
             sight=False
             break
-        open=stack.pop()
-        if open=="(" and i==")":
-            continue
-        if open=="{" and i=="}":
-            continue
-        if open=="[" and i=="]":
-            continue
-        sight=False
-        break
+else:
+    print("Ошибка")
 if sight and len(stack)==0:
     print("Баланс")
 else:

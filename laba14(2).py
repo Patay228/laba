bd4ab228d9ff56ffcc210978fb7aeb34cfd1cead
Vladimir Pashkovskiy <dedytubbe@gmail.com>
2023-11-14 15:25:43 +0300

    laba14(2)

--- laba14(2).py
+++ laba14(2).py
@@ -1,4 +1,15 @@
-s=input()
+def f(y,start,stop):
+    result=[]
+    for q in range(start,stop):
+        result.append(y[q])
+    return result
+def F(g):
+    if len(g)==0:
+        return None
+    last_element=g[-1]
+    g[:]=g[:-1]
+    return last_element
+s=input("Введите текст со скобками")
 if "(" in s or ")" in s or "[" in s or "]" in s or "{" in s or "}" in s:
     stack=[]
     sight=True
@@ -9,7 +20,7 @@ if "(" in s or ")" in s or "[" in s or "]" in s or "{" in s or "}" in s:
             if len(stack)==0:
                 sight=False
                 break
-            open=stack.pop()
+            open=F(stack)
             if open=="(" and i==")":
                 continue
             if open=="{" and i=="}":
@@ -19,7 +30,7 @@ if "(" in s or ")" in s or "[" in s or "]" in s or "{" in s or "}" in s:
             sight=False
             break
 else:
-    print("Ошибка")
+    print("NOOOOOOOOOOO")
 if sight and len(stack)==0:
     print("Баланс")
 else:
